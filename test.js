34e73e7a9e90aac66e4f40ad328de295d2daee64
MrCorley <69324197+MrCorley@users.noreply.github.com>
2020-08-13 13:51:28 -0500

    Create test.js

---
+++ test.js
@@ -0,0 +1,8 @@
+//This is an example of an assignment 
+
+//find the total
+const arrayOfNumbers = [1,2,3,4,5,6,7,8,9]
+
+const sum = arrayOfNumbers.reduce((total, currentNum) => total += currentNum)
+
+console.log(sum)
